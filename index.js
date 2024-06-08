d2ad04d53886456e8ce7942b1097d45ef310f987
rahmaidle <rahmaidle254@gmail.com>
2024-06-08 14:11:09 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    // todo
+
+  return string.toUpperCase();
+  }
+  function whisper(string) {
+
+    return string.toLowerCase();
+  }
+  function logShout(string){
+    console.log(string.toUpperCase());
+  }
+  logShout("hello");
+  function logWhisper(string){
+    console.log(string.toLowerCase());
+  }
+  logWhisper("HELLO");
+  function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase()){
+        return "I can't hear you!";
+    }
+    else if (string === string.toUpperCase()){
+        return  "YES INDEED!";
+
+    }
+    else{
+        return "I would love to!";
+    }
+  }
